a063b85f30f473cbd1764fca42ae77b5237351fc
Tianchi (David) Liu <55475915+tianchizhishui@users.noreply.github.com>
2020-02-14 09:35:53 -0800

    added title and background image

---
+++ src/PDTitle.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PDTitle extends Component {
+    render() {
+        return (
+            <div className="title">
+                <h>PD Chem ITech. Corp</h>
+            </div>
+        );
+    }
+}
+
+export default PDTitle;
